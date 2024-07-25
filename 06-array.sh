38f6fbea19e408e6ef5732d17dfc9ffaa7c8f62d
Venkatesh <venkat.obagani84@gmail.com>
2024-07-25 22:33:41 +0530

    shell is updated second time

---
+++ 06-array.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# this is called array, instead of single value it can hold multiple values
+PERSONS=("Rohit" "Kohli" "Gill")
+
+# inside array we always refer values with index and start from 0
+echo "first person:${PERSONS[0]}"

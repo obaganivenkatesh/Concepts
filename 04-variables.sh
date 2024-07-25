011a65a69e1e1b53fbc9e3952243ad95072450b4
Venkatesh <venkat.obagani84@gmail.com>
2024-07-25 21:50:39 +0530

    shell is updated second time

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# this is a comment. this program develop by addition of two numbers
+NUMBER1=$1
+NUMBER2=$2
+
+# I need to run a command inside the shell to add these 2 two numbers
+SUM=$((NUMBER1+NUMBER2))
+
+echo "addition of two numbers :$SUM"

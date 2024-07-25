13aad7d61009aa265103aeb1a390706e1edf3330
Venkatesh <venkat.obagani84@gmail.com>
2024-07-25 21:40:36 +0530

    shell is updated second time

--- 03-Variables.sh
+++ 03-Variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Hello World, I am learning aws with devops"
-PERSON1=$1;
-PERSON2=$2;
+PERSON1=$1; # this is the first value from command line
+PERSON2=$2; # this is the second value from command line
 
 echo "$PERSON1: Hi $PERSON2, good morning"
 echo "$PERSON2: Hi $PERSON1, Good morning"

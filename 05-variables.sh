84634208bf1489860c2ed8d7144c8c5dd0a4fa47
Venkatesh <venkat.obagani84@gmail.com>
2024-07-25 22:09:51 +0530

    shell is updated second time

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,12 +2,12 @@
 
 echo "please enter your username"
 
-read USERNAME # this is the variable where the value of one user entered
+read -s USERNAME # this is the variable where the value of one user entered
 
 echo "User name  entered is:$USERNAME"
 
 echo "please enter your password"
 
-read PASSWORD # this is the variable , where the password is entered.
+read -s PASSWORD # this is the variable , where the password is entered.
 
 echo "password entered is:$PASSWORD"

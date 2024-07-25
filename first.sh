c7e075a4bf803a8a8a3e033344a36c73a97f8489
Venkatesh <venkat.obagani84@gmail.com>
2024-07-25 20:34:27 +0530

    shell is updated second time

--- first.sh
+++ first.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-echo "Hello World"
-echo "Ramesh: Hi suresh, good morning"
-echo "Suresh: Hi Ramesh, Good morning"
-echo "Ramesh: How are u suresh"
-echo "Suresh: I am good , how are u"
+echo "Hello World, I am learning aws with devops"
+PERSON1=Ramesh;
+PERSON2=Suresh:
+
+echo "$PERSON1: Hi $PERSON2, good morning"
+echo "$PERSON2: Hi $PERSON1, Good morning"
+echo "$PERSON1: How are u $PERSON2"
+echo "$PERSON2: I am good , how are u"

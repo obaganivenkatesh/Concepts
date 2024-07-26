db68cd2113b22154fd76bd27333543f7fdb18362
Venkatesh <venkat.obagani84@gmail.com>
2024-07-26 17:02:47 +0530

    added the condition

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+USERID=$(id -u)
+
+R="\e[31m"
+N="\e[0m"
+
+if [ $USERID -ne o ];
+  then
+    echo -e "$R ERROR :: Please run this script with root access $N"
+    exit 1
+fi
+
 # all arguments are in $@
 for i in $@
 do
   yum install $i -y
 done 
+
+# improvements
+# implement log files
+# implement colors
+# implement validations
